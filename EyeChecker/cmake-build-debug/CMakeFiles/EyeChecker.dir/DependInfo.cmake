4867e7693622ed709ea90f9be686ede45de31cd3
yuchenlichuck <yuchenlichuck@126.com>
2018-12-10 17:02:14 +0800

    c++ object-oriented

---
+++ EyeChecker/cmake-build-debug/CMakeFiles/EyeChecker.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/code/EyeChecker/EyeChecker/EyeChecker.cpp" "D:/code/EyeChecker/EyeChecker/cmake-build-debug/CMakeFiles/EyeChecker.dir/EyeChecker.cpp.obj"
+  "D:/code/EyeChecker/EyeChecker/mmm.cpp" "D:/code/EyeChecker/EyeChecker/cmake-build-debug/CMakeFiles/EyeChecker.dir/mmm.cpp.obj"
+  "D:/code/EyeChecker/EyeChecker/pch.cpp" "D:/code/EyeChecker/EyeChecker/cmake-build-debug/CMakeFiles/EyeChecker.dir/pch.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  "../x64"
+  "../x64/Debug"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ EyeChecker/cmake-build-debug/CMakeFiles/EyeChecker.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EyeChecker.dir/EyeChecker.cpp.obj"
+  "CMakeFiles/EyeChecker.dir/mmm.cpp.obj"
+  "CMakeFiles/EyeChecker.dir/pch.cpp.obj"
+  "EyeChecker.pdb"
+  "EyeChecker.exe"
+  "EyeChecker.exe.manifest"
+  "libEyeChecker.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/EyeChecker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
